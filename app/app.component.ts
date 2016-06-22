95d090a8e48f14f4ce19b42711ff33c78498fa11
Neil-Ni <tzuyangni@gmail.com>
2016-06-22 00:15:44 -0400

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component }                   from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  directives: [],
+  templateUrl: 'app/app.component.html'
+})
+
+export class AppComponent {}
