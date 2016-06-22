084caa59083664e91ab7dece83b58a3dcd29dc62
Neil-Ni <tzuyangni@gmail.com>
2016-06-22 01:07:38 -0400

    add system scss loader

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,12 @@
+import styles from './main.scss!';
 import { Component }                   from '@angular/core';
 
 @Component({
   selector: 'my-app',
   directives: [],
+  styles: [styles],
   templateUrl: 'app/app.component.html'
 })
+
 
 export class AppComponent {}
